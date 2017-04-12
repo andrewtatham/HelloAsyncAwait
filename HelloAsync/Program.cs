72d4e885cd811ea1c6869ad54b1f0515e99f4ee9
Andrew Tatham <andrewtatham@gmail.com>
2017-04-12 12:45:19 +0100

    Added basic async/await example

--- HelloAsync/Program.cs
+++ HelloAsync/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloAsync
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            DoAsyncShenanigans();
+            Console.ReadKey();
+        }
+
+        private static async void DoAsyncShenanigans()
+        {
+            // tasks started here
+            Task<string> aTask = AAsync();
+            Task<string> bTask = BAsync();
+
+            // do something else on the current thread while the tasks are running 
+            for (int i = 0; i < 5; i++)
+            {
+                await Task.Delay(100);
+                Console.WriteLine($"C: {i}");
+            }
+
+            // wait for both tasks to complete
+            string resultA = await aTask;
+            string resultB = await bTask;
+
+            Console.WriteLine($"{resultA}, {resultB}");
+        }
+
+        private static async Task<string> BAsync()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                await Task.Delay(250);
+                Console.WriteLine($"B: {i}");
+            }
+            return "B Result";
+        }
+
+        private static async Task<string> AAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(500);
+                Console.WriteLine($"A: {i}");
+            }
+            return "A Result";
         }
     }
 }
